c4217c68e6c697ca9ef82995355921d96cbfc1eb
Sanjay Singh <itssanjay@gmail.com>
2015-08-29 13:15:48 -0400

    msmq install

---
+++ install-msmq.ps1
@@ -0,0 +1,4 @@
+﻿ForEach($featureName in Get-WindowsOptionalFeature –Online  | ? FeatureName -match "msmq" | select FeatureName)
+{
+    Enable-WindowsOptionalFeature -Online -FeatureName $featureName.FeatureName -ALL;
+}
